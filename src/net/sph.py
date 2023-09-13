ce5d239b08b226ee39cd6a381ffed881239e5f20
Thilo Molitor <thilo@eightysoft.de>
2023-09-13 14:54:11 +0200

    debug commit

--- src/net/sph.py
+++ src/net/sph.py
@@ -46,12 +46,24 @@ class SPH(Base):
         logger.debug("Uploading bytes: %s" % bytes(data, "ISO-8859-1"))
         with io.BytesIO(bytes(data, "ISO-8859-1")) as fp:
             logger.debug("Read from bytesio: %s" % fp.read(1048000))
+        data = """Datum;Stunde;AbsenterLehrer;VertretenderLehrer;Fach;Vertretungsfach;Raum;Vertretungsraum;Klassen;TextZurVertretung;Vertretungsklassen;Art;Vertretungsart;LetzteAenderung
+20230913;4;HEL;EL;WPU;;Reli;Reli;07C~08C;;07C~08C;;;202309120715
+20230913;1;HEL;RE;;;;;;;;17;B;202309120714
+20230914;1;SH;;E12ge.11;;KP12;;E12;eigenst. Arbeiten;E12;0,21;C;202309131010
+20230914;2;SH;;E12ge.11;;KP12;;E12;eigenst. Arbeiten;E12;0,21;C;202309131010
+20230914;3;SH;;Bereit;;;;;;;21;;202309131007
+20230914;5;SH;;Q12gbio.23;;BUE;;Q12;eigenst. Arbeiten;Q12;0,21;C;202309131010
+20230914;6;SH;;Q12gbio.23;;BUE;;Q12;eigenst. Arbeiten;Q12;0,21;C;202309131010
+20230914;3;SH;BOE;;;;;;;;17,21;B;202309131009
+20230914;5;FÖ;LÖL;RKA;;MW26;MW26;05A~05B~05C;;05A~05B~05C;21;;202309131413
+20230914;7;WIN;BER;Kl-St;;MW23;MW23;10B;;10B;21;;202309131434
+"""
         response = requests.post(SettingsSingleton().get_sph("url"), data={
             "i": SettingsSingleton().get_sph("schulid"),
             "c": SettingsSingleton().get_sph("uploadKey"),
             "a": "untis-dif-manuell",
             "upload": "1",
-        }, files={"d": io.BytesIO(bytes(data, "ISO-8859-1"))})      # convert to ISO-8859-1 (needed by sph)
+        }, files={"d": io.StringIO(data)})      # convert to ISO-8859-1 (needed by sph)
         if response.status_code != 200:
             self._report_http_error("Failed to upload Vertretungsplan", response.status_code, response.reason)
         result = response.text.split("|")
